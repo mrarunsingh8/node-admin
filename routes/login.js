3840b969e40fc6d4aa0a75bc0cce285e95d47870
mrarunsingh8 <mrarunsingh8@gmail.com>
2018-02-13 18:27:51 +0530

    login layout

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
-    res.render("partial/login", );
+    res.render("partial/login", {layout: 'login-layout.ejs'});
 });
 
 module.exports = router;
